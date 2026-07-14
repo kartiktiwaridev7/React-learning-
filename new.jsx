3a3343e866510ccb20246c870b6f21b91e97389d
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-07-14 23:57:03 +0530

    Add files via upload

---
+++ new.jsx
@@ -0,0 +1,11 @@
+function ProjectHeader() {
+    return (
+        < div className="banner">
+            <h1>
+                Welcome to my Business world 
+                </h1>
+            <p>I hope you like it and you gain amazing experience </p>
+        
+        </div>
+    );
+}
